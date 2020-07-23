fdae5c1fea13868860009a63457411bfefefad88
Osong Agberndifor <agberndifor7@gmail.com>
2020-07-23 10:01:17 +0100

    Add UI for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,43 @@
 class UsersController < ApplicationController
+
   def homepage
     
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save 
+      flash[:success] = 'You account has been successfully created'
+      redirect_to @user 
+    else
+      flash.now[:alert] = 'Username already taken'
+      render :new 
+    end
+  end
+
+  def show 
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    flash[:success] = 'You account has been successfully updated'
+    redirect_to @user
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:username, :image)
   end
   
 end
