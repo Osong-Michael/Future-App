a0488dbec63740be13a0bce5d119939ee9b14873
Osong Agberndifor <agberndifor7@gmail.com>
2020-07-23 10:34:21 +0100

    Add session controller to manage authentication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,31 @@
+class SessionsController < ApplicationController
+  before_action :already_logged_in, only: [:new, :create]
+  
+  def new; end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "You are signed in #{user.username}"
+    else
+      flash.now[:notice] = 'Your are not a user'
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+
+    redirect_to login_path, notice: 'You signed out'
+  end
+
+  private 
+
+  def already_logged_in
+    if logged_in?
+      flash[:error] = 'Your are already logged in'
+      redirect_to root_path 
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'users#homepage'
   resources :users
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
