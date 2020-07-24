58e42609b98c5e9e37af012abb39fafbc65cfa7f
Osong Agberndifor <agberndifor7@gmail.com>
2020-07-24 17:28:33 +0100

    Removed comments in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,4 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
